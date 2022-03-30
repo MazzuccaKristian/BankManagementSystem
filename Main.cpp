fd027ac4b7d61c220f324fe45d72e8a709ec5c9b
Kristian Mazzucca <mazzuccakristian@gmail.com>
2022-03-30 18:21:50 +0200

    Prototype. DB connection added, working.

--- Main.cpp
+++ Main.cpp
@@ -2,34 +2,41 @@
 
 int main(){
     bool isStillWorking = true;
-    int choice;
+    int userChoice;
+    int userID = -1;
+    bool isUserLogged = false;
 
-    Setup();
+    sql::Driver *db_driver;
+    sql::Connection *db_connection;
+    db_driver = get_driver_instance();
+    db_connection = db_driver -> connect(DB_HOST, DB_USER, DB_PPWW);
+
+    cout << "Connecting. Please, wait..." << endl;
+    DBSetup(db_connection);
+    if(!db_connection -> isValid()){
+        perror("ERROR: can't connect to database");
+        return 1;
+    }
 
     do{
         ShowMainMenu();
-        choice = GetUserInput();
-        switch(choice){
+        userChoice = GetUserChoice();
+        switch(userChoice){
+            case 0:
+                cout << "Program ended. Please, wait..." << endl;
+                delete db_connection;
+                isStillWorking = false;
+                break;
+
             case 1:
-                OpenAccount();
-                break;
-
-            case 2:
-                ShowBalance();
-                break;
-
-            case 3:
-                NewDeposit();
-                break;
-
-            case 4:
-                Withdraw();
-                break;
-
-            case 5:
-                Exit();
+                if(!isUserLogged){
+                    userID = LoginUser();
+                }else{
+                    //TODO: user is logged in, start OpenNewAccount phase.
+                }
         }
     }while(isStillWorking);
+
     return 0;
 }
 
