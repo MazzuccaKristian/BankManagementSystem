fa54b08b2e3580d87d43e1c8ca4cc73df82f394b
Kristian Mazzucca <mazzuccakristian@gmail.com>
2022-04-06 19:32:34 +0200

    New file to store DB params and queries. Proto.

---
+++ DB_utils.h
@@ -0,0 +1,10 @@
+#include <string>
+
+// Params for DB connection
+const std::string db_selectedschema = "BankDB";
+const std::string db_user = "bankuser";
+const std::string db_host_complete = "tcp://127.0.0.1:3306";
+const std::string db_password = "password";
+
+// Queries
+
