6bf1f9c1c2095066993bb4328d058abcc8062e2d
Kristian Mazzucca <mazzuccakristian@gmail.com>
2022-04-06 19:33:51 +0200

    Connection fixed. Case 2 fixed. Cases 3 & 4 added.

--- Main.cpp
+++ Main.cpp
@@ -3,19 +3,23 @@
 int main(){
     bool isStillWorking = true;
     int userChoice;
-    int userID = -1;
+    int userID = -1; // default value, MUST be updated with login/registration.
     bool isUserLogged = false;
 
+    // DB connection
     sql::Driver *db_driver;
     sql::Connection *db_connection;
-    db_driver = get_driver_instance();
-    db_connection = db_driver -> connect(db_host_complete, db_user, db_password);
-
-    cout << "Connecting. Please, wait..." << endl;
-    DBSetup(db_connection);
-    if(!db_connection -> isValid()){
-        perror("ERROR: can't connect to database");
-        return 1;
+    try{
+        db_driver = get_driver_instance();
+        cout << "Connecting...";
+        db_connection = db_driver -> connect(db_host_complete, db_user, db_password);
+        if(db_connection -> isValid()){
+            db_connection -> setSchema(db_selectedschema);
+            cout << " OK" << endl;
+        }
+    }catch(sql::SQLException e){
+        perror(e.what());
+        exit(EXIT_FAILURE);
     }
 
     do{
@@ -31,18 +35,44 @@ int main(){
             case 1:
                 if(!isUserLogged){
                     userID = StartLoginPhase(db_connection);
+                    isUserLogged = true;
                     cout << "Welcome back!" << endl;
-                }else{
-                    //user's logged in and his account already exists.
-                    // OpenNewAccount();
                 }
                 break;
 
             case 2:
+                if(!isUserLogged){
+                    userID = StartLoginPhase(db_connection);
+                    isUserLogged = true;
+                }
                 ShowBalance(userID, db_connection);
+                break;
+
+            case 3:
+                if(!isUserLogged){
+                    userID = StartLoginPhase(db_connection);
+                    isUserLogged = true;
+                }
+                cout << "Enter the amount you want to deposit: ";
+                //TODO: possible check on amount (positive-only? non-zero?)
+                double deposit;
+                cin >> deposit;
+                MakeNewDeposit(userID, deposit, db_connection);
+                break;
+
+            case 4:
+                if(!isUserLogged){
+                    userID = StartLoginPhase(db_connection);
+                    isUserLogged = true;
+                }
+                cout << "Enter the amount you want to withdraw: ";
+                double amount;
+                cin >> amount;
+                Withdraw(userID, amount, db_connection);
+                break;
         }
     }while(isStillWorking);
 
-    return 0;
+    return EXIT_SUCCESS;
 }
 
