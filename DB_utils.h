99a48fd7143a008cfbefbd0a1f6e719ad96a101c
Kristian Mazzucca <mazzuccakristian@gmail.com>
2022-04-08 19:14:27 +0200

    Queries added. Used as sql::PreparedStatement.

--- DB_utils.h
+++ DB_utils.h
@@ -7,4 +7,7 @@ const std::string db_host_complete = "tcp://127.0.0.1:3306";
 const std::string db_password = "password";
 
 // Queries
-
+const std::string login_query = "SELECT Id FROM Users WHERE Username=? AND Password=?";
+const std::string registration_query = "INSERT INTO Users(Username, Password) VALUES (?,?)";
+const std::string showBalance_query = "SELECT Balance FROM Users WHERE Id=?";
+const std::string updateBalance_query = "UPDATE Users SET Balance=? WHERE Id=?";
